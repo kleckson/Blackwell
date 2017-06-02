1bf2ab87bb89ad1ade9d89ea4c1c0af142899409
Tobias <tobiashakanthoren@gmail.com>
2017-06-02 17:58:17 +0200

    initial commit

---
+++ libGDX/desktop/src/com/blackwell/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blackwell.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blackwell.game.BGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BGame(), config);
+	}
+}
